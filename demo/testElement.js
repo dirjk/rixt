2f56d87f81211a0c67e052cdc053ed6ea0a9c476
Joe Ryan <joe.ryan@creativeapestudios.com>
2022-03-03 10:54:52 -0600

    can now import secondary elements and attach event functions to the dom

--- demo/testElement.js
+++ demo/testElement.js
@@ -2,12 +2,20 @@
 /** @jsx rixt */
 import rixt from '../src/index.js'
 
-const testElement = () => (
+const Hello = () => (
+    <p>hello</p>
+)
+const testElement = () => {
+    return (
     <div data={'test'} id="myID">
         testElement
         <p fuck={'you'}>ptest</p>
         2ndtestElement
+        <Hello/>
+        <button onclick={() => {
+            alert('ok')
+        }}>test button</button>
     </div>
-)
+)}
 
 export default testElement
--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ function rixt(tag, props, ...children){
 
 function recursiveMount(elementObj) {
     const element = document.createElement(elementObj.tag)
-    Object.keys(elementObj.props).forEach(key => {
-        console.log('key', key)
-        element.setAttribute(key, elementObj.props[key])
-    })
+    if (elementObj.props !== null) {
+        Object.keys(elementObj.props).forEach(key => {
+            const attr = elementObj.props[key]
+            if (typeof attr === 'function') {
+                element[key] = attr
+            } else {
+                element.setAttribute(key, elementObj.props[key])
+            }
+        })
+    }
     elementObj.children.forEach(child => {
         if (typeof child === 'string') {
             element.appendChild(document.createTextNode(child))
+        } else if (typeof child.tag === 'function') {
+            const newElement = child.tag()
+            element.appendChild(recursiveMount(newElement))
         } else {
             element.appendChild(recursiveMount(child))
         }
